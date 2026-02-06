b09b84520bbcccf74a7b6ea13fa6e3b9e2561d58
mdabdulawal2001 <mdabdulawal2001@gmail.com>
2026-02-07 00:19:29 +0600

    added problem1

--- problem1.js
+++ problem1.js
@@ -1,1 +1,18 @@
-alert("I am Toha");
+function newPrice(currentPrice, discount) {
+  if (
+    typeof currentPrice !== "number" ||
+    typeof discount !== "number" ||
+    discount < 0 ||
+    discount > 100
+  ) {
+    return "Invalid";
+  }
+
+  const discountedPrice = (currentPrice * discount) / 100;
+  const payablePrice = currentPrice - discountedPrice;
+
+  return payablePrice.toFixed(3);
+}
+
+const result = newPrice(2000, 17.17);
+console.log(result);
